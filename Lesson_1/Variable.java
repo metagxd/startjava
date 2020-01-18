911fcfe0394595e37bfea5ac411b722a2f8505df
Artyom Vasilyev <metagxd@gmail.com>
2020-01-18 12:49:51 +0300

    implement Variable.java

---
+++ Lesson_1/Variable.java
@@ -0,0 +1,16 @@
+public class Variable {
+    public static void main(String[] args) {
+        byte cores = 8;
+        short threads = 16;
+        float freq = 4.2f;
+        long mem = 32768;
+        int gpu_mem = 8092;
+        double gpu_freq = 1.9;
+        char gpu_mem_freq = 8000; //не выводиться значение, только знак вопроса
+        boolean isWarranty = true;
+        System.out.println("Количество ядер: " + cores + "\n" + "Количество потоков " + threads + "\n" + 
+            "Частота процессора: " + freq + " GHz \n" + "Количество оперативной памяти : " + mem + " MB\n" + 
+            "Количество видеопамяти " + gpu_mem + " MB\n" + "Частота видеоядра " + gpu_freq + " GHz\n" + 
+            "Частота видеопамяти " + gpu_mem_freq + " MHz\n" + "На грантии: " + isWarranty);
+    }
+}
