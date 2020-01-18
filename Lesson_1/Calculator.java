56069d03513bddacfe0ece4be6c8c09e47ba36d3
Artyom Vasilyev <metagxd@gmail.com>
2020-01-18 15:19:53 +0300

    Implement Calculator.java

---
+++ Lesson_1/Calculator.java
@@ -0,0 +1,31 @@
+public class Calculator {
+    public static void main(String[] args) {
+        int x = 3;
+        int y = 0;
+        int res = 1;
+        String op = "^";
+        if (x > 0 && y > 0 ) {//проверка входных данных
+            if (op.equals("+")) {
+                res = x + y;
+                System.out.println(res);
+            } else if (op.equals("-")) {
+                res = x-y;
+                System.out.println(res);
+            } else if (op.equals("*")) {
+                res = x * y;
+                System.out.println(res);
+            } else if (op.equals("/")) {
+                res = x / y;
+                System.out.println(res);
+            } else if (op.equals("^")) {
+                for (int i = 1;i <= y ;i++) {//цикл возведения в степень
+                    res *= x;
+                }
+                System.out.println(res);
+            } else if (op.equals("%")) {
+                res = x % y;
+                System.out.println(res);
+            }
+        } else System.out.println("Ошибка!");
+    }
+}
