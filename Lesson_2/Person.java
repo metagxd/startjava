995c76b7748a72d56b482c3b8a385d80c0959dff
Artyom Vasilyev <metagxd@gmail.com>
2020-01-19 15:43:29 +0300

    implement Lesson_2

---
+++ Lesson_2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+
+    String name = "Alex";
+    String sex = "male";
+    int age = 30;
+    int weight = 80;
+    int height = 190;
+
+    void walk() {
+
+    }
+
+    void seat() {
+
+    }
+
+    boolean run() {
+        return true;
+    }
+
+    String talk() {
+        return "Hello!";
+    }
+
+    boolean learnJava() {
+        return true;
+    }
+}
