809f72164f409ca6c27d4a4bbe6e5aca82e7c1ae
Artyom Vasilyev <metagxd@gmail.com>
2020-01-22 05:50:50 +0300

    implement Jaeger.java; JaegerTest.java

---
+++ Lesson_2/Jaeger.java
@@ -0,0 +1,76 @@
+public class Jaeger {
+
+    private String name;
+    private String mark;
+    private String origin;
+    private int height;
+    private int weight;
+    private int speed;
+    private int strenght;
+    private int armor;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setStrenght(int strenght) {
+        this.strenght = strenght;
+    }
+
+    public int getStrenght() {
+        return strenght;
+    }
+
+    public void setArmor(int armor) {
+        this.armor = armor;
+    }
+
+    public int getArmor() {
+        return armor;
+    }
+
+}
---
+++ Lesson_2/JaegerTest.java
@@ -0,0 +1,42 @@
+public class JaegerTest{
+
+    public static void main(String[] args) {
+        Jaeger jaeger1 = new Jaeger();
+        Jaeger jaeger2 = new Jaeger();
+
+        jaeger1.setName("Striker Eureka");
+        jaeger2.setName("Bracer Phoenix");
+        jaeger1.setMark("Mark-5");
+        jaeger2.setMark(jaeger1.getMark());
+        jaeger1.setOrigin("Australia");
+        jaeger2.setOrigin("China");
+        jaeger1.setHeight(76);
+        jaeger2.setHeight(jaeger1.getHeight());
+        jaeger1.setWeight(1850);
+        jaeger2.setWeight(2128);
+        jaeger1.setSpeed(10);
+        jaeger2.setSpeed(9);
+        jaeger1.setStrenght(9);
+        jaeger2.setStrenght(jaeger1.getStrenght());
+        jaeger1.setArmor(9);
+        jaeger2.setArmor(10);
+
+        System.out.println("Jaeger 1:" + "\nName: " + jaeger1.getName() + "\nMark: " +
+        jaeger1.getMark() + "\nOrigin: " + jaeger1.getOrigin() + "\nHeight: " +
+        jaeger1.getHeight() + "\nWeight: " + jaeger1.getWeight() + "\nSpeed: " +
+        jaeger1.getSpeed() + "\nSternght: " + jaeger1.getStrenght() + "\nArmor: " +
+        jaeger1.getArmor());
+
+        System.out.println("\nJaeger 2:" + "\nName: " + jaeger2.getName() + "\nMark: " +
+        jaeger2.getMark() + "\nOrigin: " + jaeger2.getOrigin() + "\nHeight: " +
+        jaeger2.getHeight() + "\nWeight: " + jaeger2.getWeight() + "\nSpeed: " +
+        jaeger2.getSpeed() + "\nSternght: " + jaeger2.getStrenght() + "\nArmor: " +
+        jaeger2.getArmor());
+
+        jaeger2.setWeight(2000);
+        System.out.println(jaeger2.getName() + " weight: " + jaeger2.getWeight());
+
+        jaeger1.setSpeed(8);
+        System.out.println(jaeger1.getName() + " speed: " + jaeger1.getSpeed());
+    }
+}
