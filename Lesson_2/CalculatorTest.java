171aac446ee8c7cafcddcbf2ff5cedd585e1aa99
Васильев Артем Андреевич <VasilyevAA@ad.virial.ru>
2020-01-27 12:33:24 +0300

    "[Yes/No]" fix.

--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -6,48 +6,49 @@ public class CalculatorTest {
         Calculator calculator = new Calculator();
         Scanner input = new Scanner(System.in);
         int number = 0;
-
-        do {
-            System.out.print("Type first number: ");
-            if (input.hasNextInt()) {
-                number = input.nextInt();
-            } else {
-                System.out.println("Error!");
-                input.next();
-            }
-        } while (!calculator.setFirstNumber(number));
-
-        do {
-            System.out.print("Type operation: ");
-        } while (!(calculator.setOperation((input.next()).charAt(0))));
-
-        do {
-            System.out.print("Type second number: ");
-            if (input.hasNextInt()) {
-                number = input.nextInt();
-            } else {
-                System.out.println("Error!");
-                number = 0;
-                input.next();
-            }
-        } while (!calculator.setSecondNumber(number));
-
         String answer;
-        do {
-            System.out.print("Continue? [Yes/No]: ");
-            answer = input.next();
 
-            switch (answer) {
-            case "No":
-                System.out.println("Aborting...");
-                break;
-            case "Yes":
-                System.out.println("Result: " + calculator.calculate());
-                break;
-            default:
-                System.out.println("Try again.");
-                break;
-            }
-        } while (!("Yes".equals(answer) || "No".equals(answer)));
+        do {
+            do {
+                System.out.print("Type first number: ");
+                if (input.hasNextInt()) {
+                    number = input.nextInt();
+                } else {
+                    System.out.println("Error!");
+                    input.next();
+                }
+            } while (!calculator.setFirstNumber(number));
+
+            do {
+                System.out.print("Type operation: ");
+            } while (!(calculator.setOperation((input.next()).charAt(0))));
+
+            do {
+                System.out.print("Type second number: ");
+                if (input.hasNextInt()) {
+                    number = input.nextInt();
+                } else {
+                    System.out.println("Error!");
+                    number = 0;
+                    input.next();
+                }
+            } while (!calculator.setSecondNumber(number));
+            System.out.println("Result: " + calculator.calculate());
+
+            do {
+                System.out.print("Continue? [Yes/No]: ");
+                answer = input.next();
+                switch (answer) {
+                case "No":
+                    System.out.println("Close...");
+                    break;
+                case "Yes":
+                    break;
+                default:
+                    System.out.println("Try again.");
+                    break;
+                }
+            } while (!("No".equals(answer) || "Yes".equals(answer)));
+        } while (!"No".equals(answer));
     }
 }
